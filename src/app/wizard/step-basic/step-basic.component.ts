d88e12dda4fafd8d16e91f25d1a74807a3c0fd60
Basit Munir <basitmunir.pk@gmail.com>
2020-07-02 20:31:55 +0500

    validation implemented

--- src/app/wizard/step-basic/step-basic.component.ts
+++ src/app/wizard/step-basic/step-basic.component.ts
@@ -23,8 +23,8 @@ export class StepBasicComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe(params => {
       console.log(params);
       this.ID = params["id"];
-      //this._http._get('workflow?id='+this.ID).subscribe(
       this._http._get('login-input.json?id='+this.ID).subscribe(
+      //this._http._get('workflow?ModelOnly=true&id='+this.ID).subscribe(
         response => {
           console.log(response);
           this.formBuilder = response;
@@ -35,14 +35,34 @@ export class StepBasicComponent implements OnInit {
   }
 
   submitForm () {
-    this.step++;
-    this._http._get('workflow.json').subscribe(
-      response => {
-        console.log(this.step);
-        this.formBuilder = response[this.step];
-      },
-      err => {}
-    )
+    console.log(this.formBuilder);
+    if (this.inputValidated()) {
+      this.step++;
+      this._http._get('workflow.json').subscribe(
+      //this._http._post('ContinueWorkflowModelOnly', this.formBuilder).subscribe(
+        response => {
+          console.log(this.step);
+          this.formBuilder = response[this.step];
+        },
+        err => {
+        }
+      )
+    }
+  }
+
+  inputValidated () {
+    let validated = true;
+    this.formBuilder.Controls.filter(function(field){
+      if((field.VisibleFields == 4 || field.VisibleFields == 5) && field.IsRequired) {
+        if(field.ModifiedFormattedValue == '') {
+          field.errorMessage = "Field is Required";
+          validated = false;
+        } else {
+          field.errorMessage = "";
+        }
+      }
+    });
+    return validated;
   }
 
 }
